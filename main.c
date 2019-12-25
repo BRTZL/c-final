9b271877823bc9a08a10aa5c29ef2a1b44afbda1
Bartu <bartuozel@hotmail.com>
2019-12-25 23:56:34 +0300

    fresh start

--- main.c
+++ main.c
@@ -12,110 +12,219 @@ typedef struct complex_number
 } complex;
 
 complex add(complex n1, complex n2);
-complex exp_calc(char str[MAXLINE], complex number);
-complex norm_calc(char str[MAXLINE], complex number);
 
-int matrix();
-int expression();
-void print_array(char str[]);
-complex euler(char str[MAXLINE]);
-void strConversion(char str[MAXLINE][MAXLINE], int term);
+void print_array(char str_chr[]);
+void print_int(double str_int[]);
+char *ready_to_compile(char str[]);
+double *compile(char str[]);
+int get_index(char str[], int space);
+
+int main()
+{
+    int option = 0, approval = 0;
+    int debug = 1;
+    char equation[MAXLINE];
+
+    char approved[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.', '0', '+', '-', '(', ')', ' ', 'e', 'x', 'p', 'j', 'i'};
+
+    printf("Welcome to our COPMLEX NUMBER CALCULATOR");
+    printf("\nby ....");
+    printf("\n\nBefore continue please enter which mode you want to use: ");
+    printf("\n\t1. Expression mode (default)");
+    printf("\n\t2. Matrix mode\n : ");
+    if (!debug)
+        scanf("%d", &option);
+
+    if (debug)
+        option = 1;
+
+    while (option != 1 && option != 2)
+    {
+        fflush(stdin);
+        printf("\nYou entered invalid choose");
+        printf("\n\nBefore continue please enter which mode you want to use: ");
+        printf("\n\t1. Expression mode (default)");
+        printf("\n\t2. Matrix mode\n : ");
+        scanf("%d", &option);
+    }
+
+    while (approval == 0)
+    {
+        fflush(stdin);
+        printf("Please type your equation: ");
+        fflush(stdin);
+        memset(&equation[0], 0, sizeof(equation));
+        gets(equation);
+
+        for (unsigned long i = 0; i < strlen(equation); i++)
+        {
+            if ((equation[i] == '-' && equation[i + 1] == '-') || (equation[i] == '+' && equation[i + 1] == '+'))
+            {
+                printf("\nYou entered two operator in a row -> %c%c", equation[i], equation[i + 1]);
+                printf("\n                            index no ^^");
+                printf("\n                                     %lu%lu", i / 10, (i + 1) / 10);
+                printf("\n                                     %lu%lu\n", i % 10, (i + 1) % 10);
+                approval = 0;
+                break;
+            }
+            else if (strchr(approved, equation[i]) == NULL)
+            {
+                printf("\nYou entered unapproved character -> %c", equation[i]);
+                printf("\n                           index no ^");
+                printf("\n                                    %lu", i / 10);
+                printf("\n                                    %lu\n", i % 10);
+                approval = 0;
+                break;
+            }
+            else
+                approval = 1;
+        }
+    }
+
+    printf("\n.");
+
+    //Analyze equation and store its compenents in operation list
+
+    /*
+    1+2j+6exp(-0.3pij)+6j+9+0.23exp(-0.4j)+12j
+    */
+
+    char operation[MAXLINE][MAXLINE];
+    double result[MAXLINE][2];
+    int index1 = 0, index2 = 0, inside = 0;
+    memset(&operation[0], '\0', sizeof(operation));
+    memset(&result[0], '\0', sizeof(result));
+
+    for (unsigned long i = 0; i < strlen(equation); i++)
+    {
+        if (equation[i] == '(')
+        {
+            inside = 1;
+        }
+        else if (equation[i] == ')')
+        {
+            inside = 0;
+        }
+        else if ((equation[i] == '-' || equation[i] == '+') && inside == 0)
+        {
+            index1++;
+            index2 = 0;
+        }
+        else
+        {
+            if (index2 == 0)
+                operation[index1][index2] = (i != 0) ? ((equation[i - 1] == '-') ? '-' : '+') : '+';
+
+            operation[index1][index2 + 1] = equation[i];
+            index2++;
+        }
+    }
+
+    printf("\n..");
+    //fflush(stdout);
+    char *compiled[index1];
+    int temp_index;
+
+    for (int i = 0; i <= index1; i++)
+    {
+        compiled[i] = ready_to_compile(operation[i]);
+        temp_index = get_index(operation[i], 0);
+        result[i][temp_index] = *compile(compiled[i]);
+        fflush(stdin);
+        //printf("\n\tresult -> \'%f\'", atof(compiled[i]));
+        printf("\n\tresult -> \'%f\'", result[i][temp_index]);
+        print_array(compiled[i]);
+        printf("%d", get_index(operation[i], 0));
+    }
+
+    printf("\n...");
+
+    printf("\n\nquiting!");
+    return 0;
+}
 
 /*
-
-equation  1. layer --> 
-                        0 : number
-                        1 : complex number
-                        2 : notation 
-
--         2. layer -->  
-
+    1+2j+6exp(-0.3pij)+6j+9+0.23exp(-0.4j)+12j
 */
 
-void print_array(char str[])
+char *ready_to_compile(char str[])
 {
-    printf("\n\t array -> \'");
+    int index = 0;
+    int len = strlen(str);
+    for (int i = 0; i < len - index; i++)
+    {
+        if (str[i] == 'e' && str[i + 1] == 'x' && str[i + 2] == 'p')
+        {
+            index += 2;
+            str[i] = '*';
+        }
+        else
+        {
+            str[i] = str[i + index];
+        }
+    }
+    for (int i = len - index; i < len; i++)
+        str[i] = '\0';
+
+    len -= index;
+
+    return str;
+}
+
+double *compile(char str[])
+{
+    double op[MAXLINE];
+    memset(&op[0], '\0', sizeof(op));
+    int len = strlen(str);
+
+    op[0] = atof(str);
+
+    return op;
+}
+
+/*
+    * 0 real NUMBER
+    * 1 coplex NUMBER
+    * 2 exp function
+*/
+
+int get_index(char str[], int space)
+{
+    int state = 0;
+
     for (unsigned long i = 0; i < strlen(str); i++)
     {
-        printf("%c", str[i]);
+        if (str[i] == 'j')
+            state = 1;
+        else if (str[i] == '*')
+            state = 2;
+        else
+            state = 0;
+
+        if (state != 0)
+            break;
+    }
+    return state;
+}
+
+void print_array(char str_chr[])
+{
+    printf("\n\t array -> \'");
+    for (unsigned long i = 0; i < strlen(str_chr); i++)
+    {
+        printf("%c", str_chr[i]);
     }
     printf("\'");
 }
 
-void strConversion(char str[MAXLINE][MAXLINE], int term)
+void print_int(double str_int[])
 {
-    complex number;
-    number.real = 0;
-    number.imag = 0;
-
-    printf("\nArray -> \n");
-    for (int i = 0; i < term; i++)
+    printf("\n\t array -> \'");
+    for (unsigned long i = 0; i < sizeof(str_int); i++)
     {
-        int len = strlen(str[i]);
-        for (int j = 0; j < len; j++) //removing unnecessary spaces
-        {
-            if (str[i][j] == ' ')
-            {
-                for (int z = j; z < len; z++)
-                {
-                    str[i][z] = str[i][z + 1];
-                }
-                len--;
-            }
-        }
-
-        printf("\n\t\'%s\'", str[i]);
-
-        if (str[i][len - 1] == 'j' || str[i][len - 2] == 'j')
-            number = exp_calc(str[i], number);
-        else
-            number = norm_calc(str[i], number);
-        printf("\n\treal: %.3f imag: %.3f\n", number.real, number.imag);
+        printf("[%f]", str_int[i]);
     }
-
-    //printf("\n\n\treal: %.3f imag: %.3f", number.real, number.imag);
-}
-/*
-1+2j+6exp(-0.3pij)+6j+9+0.23exp(-0.4j)+12j
-*/
-complex norm_calc(char str[MAXLINE], complex number)
-{
-    complex temp;
-    printf("\n\t\'%.2f\'", atof(str));
-    temp.real = atof(str);
-    temp.imag = 0;
-
-    temp = add(temp, number);
-    return temp;
-}
-
-complex exp_calc(char str[MAXLINE], complex number)
-{
-    //eular calculation
-    complex temp;
-
-    printf("\n\t\'%.2fj\'", atof(str));
-    if (str[strlen(str) - 1] == 'e')
-    {
-        temp = euler(str);
-    }
-    else
-    {
-        temp.imag = atof(str);
-        temp.real = 0;
-    }
-
-    temp = add(temp, number);
-    return temp;
-}
-
-complex euler(char str[MAXLINE])
-{
-    complex temp;
-    temp.imag = 0;
-    temp.real = 0;
-
-    return temp;
+    printf("\'");
 }
 
 complex add(complex n1, complex n2)
@@ -123,123 +232,5 @@ complex add(complex n1, complex n2)
     complex temp;
     temp.real = n1.real + n2.real;
     temp.imag = n1.imag + n2.imag;
-    return (temp);
-}
-
-
-
-    ///*
-    int space = 0, func = 0, term = 0;
-    char operation[MAXLINE][MAXLINE];
-    char temp[MAXLINE], temp_exp[MAXLINE];
-    memset(&temp[0], 0, sizeof(temp));
-    memset(&operation[0][0], 0, sizeof(operation));
-
-    for (unsigned long i = 0; i < strlen(equation); i++)
-    {
-        //printf("\n\nfor loop i: %d", i);
-        if (equation[i] == '-' || equation[i] == '+' || (i == 0 && (equation[i] != '-' || equation[i] != '+')))
-        {
-            func = 0;
-            i++;
-
-            if (i == 1 && (equation[i - 1] != '-' && equation[i - 1] != '+'))
-                i--;
-
-            //memset(&temp[0], 0, sizeof(temp));
-
-            if (equation[i - 1] != '\0')
-                operation[term][0] = equation[i - 1]; //temp[0] = equation[i - 1];
-            else
-                operation[term][0] = '+'; //temp[0] = '+';
-
-            while ((equation[func + i] != '-' && equation[func + i] != '+') != (equation[func + i - 1] == '('))
-            {
-                
-                    term++;
-                    space = 0;
-                    int tfunc = 0;
-                    func += 4;
-                    //operation[term][tfunc - space] = 'e';
-                    memset(&temp_exp[0], 0, sizeof(temp_exp));
-                    while (equation[i + func + tfunc] != ')')
-                    {
-                        if (equation[i + func + tfunc] == 'p' && equation[i + func + tfunc + 1] == 'i')
-                        {
-                            operation[term][tfunc - space] = 'p';
-                            space++;
-                            tfunc += 2;
-                        }
-                        else
-                        {
-                            operation[term][tfunc - space] = equation[func + i + tfunc]; //temp_exp[tfunc] = equation[func + i + tfunc];
-                            tfunc++;
-                        }
-                    }
-                    operation[term][tfunc - space] = 'e';
-                    // printf("\n\t func exp:%d", tfunc);
-                    // print_array(operation[term]);
-                    func += tfunc;
-                }
-                else if (equation[i + func + 1] == 'e' && )
-                {
-                }
-                else
-                {
-                    operation[term][func + 1] = equation[func + i]; //temp[func + 1] = equation[func + i];
-                    func++;
-                }
-            }
-            //printf("\n\t func:%d", func);
-            //print_array(operation[term]);
-            i += func - 1;
-            term++;
-        }
-    }
-
-    strConversion(operation, term);
-
-    /*
-    real 
-    1+2j+6exp(-0.3pij)+6j+9+0.23exp(-0.4j)+12j
-    000000000011111111112222222222333333333344
-    012345678901234567890123456789012345678901
-
-    test for only numbers
-    1+2j+6-6j-9+12j
-    000000000011111
-    012345678901234
-    */
-
-    //*/
-
-    printf("\n..");
-
-    /*
-    for (int i = 0; i < MAXLINE; i++)
-    {
-        for (int j = 0; j < 2; i++)
-        {
-            printf(" %d ", operation[i][j]);
-        }
-    }
-    */
-    if (option == 1)
-        expression();
-    else
-        matrix();
-    printf("\n...");
-    return 0;
-}
-
-int expression()
-{
-
-    return 0;
-}
-
-int matrix()
-{
-
-    return 0;
+    return temp;
 }
